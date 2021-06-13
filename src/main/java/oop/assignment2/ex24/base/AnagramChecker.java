134e5c6a53699642c2cada4148bb12a3953ab008
Glenroy Little <glittle02@knights.ucf.edu>
2021-06-12 19:16:03 -0500

    Ex24 neatness

--- src/main/java/oop/assignment2/ex24/base/AnagramChecker.java
+++ src/main/java/oop/assignment2/ex24/base/AnagramChecker.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 first_name last_name
  */
-public class AnagramChecker {
+public class AnagramChecker
+{
     private static final Scanner in = new Scanner(System.in);
 
     public String s1;
--- src/main/java/oop/assignment2/ex24/base/Solution24.java
+++ src/main/java/oop/assignment2/ex24/base/Solution24.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
  *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 first_name last_name
  */
-public class Solution24 {
+public class Solution24
+{
 
     public static void main(String[] args) {
         AnagramChecker ac = new AnagramChecker();
